d55adee80a18648124753f3ec25ba72db823e33a
Bhavika Gianey <bgianey@microsoft.com>
2023-10-13 10:24:33 +0530

    controller logic

--- controllers/wbcsnapshot_controller.go
+++ controllers/wbcsnapshot_controller.go
@@ -47,14 +47,158 @@ type WbcSnapshotReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *WbcSnapshotReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	_ = r.log.WithValues("wbcsnapshot",req.NamespacedName) //accessed the namespace name of the resource
 
 	// TODO(user): your logic here
+	//Client get Function : Grab relevant WbcSnapShot resource
+	snapshot := &snapshotv1alpha1.WbcSnapshot{}
+	snapshotReadErr := r.Get(ctx,req.NamespacedName,snapshot)
+
+	//Exit early if something went wrong parsing the WbcSnapshot object
+	if snapshotReadErr != nil || len(snapshot.Name) == 0{
+		r.log.Error(snapshotReadErr,"Error encountered reading snapshot")
+		return ctrl.Result{},snapshotReadErr
+	}
+
+	snapshotCreator := &corev1.Pod{}
+	snapshotCreatorReadErr := r.Get(
+		ctx, types.NamespacedName{Name: "snapshot-creator",Namespace: req.Namespace},snapshotCreator,
+	)
+
+	//exit if wbcsnapshot creator is already running
+	if snapshotCreatorReadErr == nil{
+		r.log.Error(snapshotCreatorReadErr,"Snapshot creater already running")
+		return ctrl.Result{},snapshotCreatorReadErr
+	}
+
+
+	newPvName := "wbc-snapshot-pv-" + strconv.FormatInt(time.Now().Unix(),10) + "-" + snapshot.Spec.SourceVolumeName
+	newPvcname := "wbc-snapshot-pvc-" + strconv.FormatInt(time.Now().Unix(),10) + "-" + snapshot.Spec.SourceClaimName
+
+	//create a new Persistent Volume
+	newPersistentVol := corev1.persistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+				Name: 	   newPvtName,
+				Namespace: req.Namespace,
+		},
+		Spec: corev1.PersistentVolumeSpec{
+			    StorageClassName: "manual",
+				AccessModes: 	  []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+				Capacity: corev1.ResourceList{
+						corev1.ResourceName(corev1.ResourceStorage): resource.MustParse("10Gi"),		
+				},
+				PersistentVolumeSource: corev1.PersistentVolumtSource{
+					HostPath: &corev1.HostPathVolumeSource{
+						Path: snapshot.Spec.HostPath,
+					},
+				},
+		},
+	}
+
+
+
+
+
+
+	pvCreateErr := r.Create(ctx, &newPersistentVol)
+	if pvCreateErr != nil {
+		r.Log.Error(pvCreateErr,"Error encountered creating new pv")
+		return ctrl.Result{},pvCreateErr
+	}
+
+	_= r.Log.WithValues("Created New Snapshot Persistent Volume",req.NamespacedName)
+
+	manualStorageClass := "manual"
+
+	//create new persisitent volume connected to new persistent volume
+	newPersistentVolClaim := corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:          newPvcName,
+			Namespace:     req.Namespace,
+		},
+		Spec: corev1.PersistentVolumeSpec{
+			StorageClassName: &manualStorageClass,
+			VolumeName:		  newPvName,
+			AccessModes: 	  []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+			Resources: corev1.ResourceRequiremnts{
+				   Requests: corev1.ResourceList{
+					      corev1.ResourceName(corev1.ResourceStorage): resource.MustParse("10Gi"),		
+					},
+			},
+		},
+	}
+
+	pvcCreateErr := r.Create(ctx, &newPersistentVolClaim)
+	if pvcCreateErr !- nil{
+		r.Log.Error(pvcCreateErr,"Error encountered creating new pvc")
+		return ctrl.Result{}, pvcCreateErr
+	}
+
+	_= r.Log.WithValues("Created New Snapshot Persistent Volume claim", req.NamespacedName)
+
+
+
+	snapshotCreatorPod := corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:          "snapshot-creator",
+			Namespace:     req.Namespace,
+		},
+		Spec: corev1.PodSpec{
+			RestartPolicy: "Never",
+			Volumes: []corev1.Volume{
+				    {
+						Name: "wbc-snapshot-" + snapshot.Spec.SourceClaimName,
+						VolumeSource: corev1.VolumeSource{
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimSource{
+								ClaimName: snapshot.Spec.SourceClaimName,
+							},
+						},
+					},
+					{
+						Name: "wbc-snapshot-" + newPvcName,
+						VolumeSource: corev1.VolumeSource{
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimSource{
+								ClaimName: newPvcName,
+							},
+						},
+					},
+				},
+				Container: []corev1.Container{
+					{
+						Name: "busybox",
+						Image: "k8s.grc.io/busybox",
+						Command: []string{
+							    "/bin/sh",
+								"-c",
+								"cp /tmp/source/test.file /tmp/dest/test.file"},
+						VolumeMounts: []corev1.VolumeMount{
+							  {
+								Name: "wbc-snapshot-" + snapshot.Spec.SourceClaimName,
+								MountPath: "/tmp/source",
+							  },
+							  {
+								Name: "wbc-snapshot-" + newpvcName,
+								MountPath: "/tmp/dest",
+							  },
+						},
+					},
+				},
+		},
+	}
+
+	podCreateErr := r.Create(ctx, &snapshotCreatorPod)
+	if podCreateErr !- nil{
+		r.Log.Error(pvCreateErr,"Error encountered creating snapshot pod")
+		return ctrl.Result{}, podCreateErr
+	}
+
+	_= r.Log.WithValues("Instantiating snapshot-creator pod", req.NamespacedName)
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
+//tell the manager to watch certain resources(watch wbc snapshot resources)
 func (r *WbcSnapshotReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&snapshotv1alpha1.WbcSnapshot{}).
